af4d1ae3d4b93b0d3f0f9d34d1756764ae8b0b08
Hugo-walando <hugowalando@gmail.com>
2024-07-03 12:54:02 +0200

    chore: add colors & font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
       inter: ['Inter', 'sans-serif'],
       nunito: ['Nunito Sans', 'sans-serif'],
     },
+    colors: {
+      'color-red': '#ce0033', //Background Nav & buttons (Rouge Simplon)
+      'color-white': '#FFFFFF', //Background Cards (Blanc)
+      'color-back': '#f0f0f0', //Background body (Blanc foncé)
+      'color-select': '#f8fafc', //Background selects (Gris clair)
+      'color-black': '#000000', //Text (Noir)
+      'color-success': '#009640', //Success (Vert)
+      'color-disabled': '#c4c4c4', //Disabled (Gris)
+    },
   },
   plugins: [],
 };

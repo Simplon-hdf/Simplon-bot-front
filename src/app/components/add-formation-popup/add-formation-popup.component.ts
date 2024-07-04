0bb67e618eb949edf80fd7980817a8f77e6aa400
Hugo-walando <hugowalando@gmail.com>
2024-07-04 16:27:15 +0200

    feat: generate add-formation-popup component

---
+++ src/app/components/add-formation-popup/add-formation-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-formation-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-formation-popup.component.html',
+  styleUrl: './add-formation-popup.component.scss'
+})
+export class AddFormationPopupComponent {
+
+}

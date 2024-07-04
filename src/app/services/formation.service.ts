390c82eedde2be0147c7e37b61136afa043006cd
Hugo-walando <hugowalando@gmail.com>
2024-07-04 16:24:47 +0200

    feat: add formation service

--- src/app/services/formation.service.ts
+++ src/app/services/formation.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { FormationModel } from '../models/formation-model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FormationService {
+  private formations: FormationModel[] = [
+    {
+      id: 1,
+      name: 'Formation 1',
+      start_date: '2023-01-01',
+      end_date: '2023-01-10',
+      place: 'Paris',
+      caps: 'John Doe',
+      status: 'en cours',
+    },
+    {
+      id: 2,
+      name: 'Formation 2',
+      start_date: '2023-02-01',
+      end_date: '2023-02-10',
+      place: 'Lyon',
+      caps: 'Jane Smith',
+      status: 'terminé',
+    },
+  ];
 
-  constructor() { }
+  getFormations() {
+    return this.formations;
+  }
+
+  addFormation(formation: any) {
+    this.formations.push({ ...formation, id: this.formations.length + 1 });
+  }
 }

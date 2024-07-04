7ef64365f523bfcdc69f0d30cc8bbfdb6ad62568
Hugo-walando <hugowalando@gmail.com>
2024-07-04 11:09:08 +0200

    feat: generate formations component & add missing routerlink to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormationsComponent } from './components/formations/formations.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    DashboardComponent,
+    NavbarComponent,
+    FormationsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormationsComponent } from './components/formations/formations.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'formations', component: FormationsComponent },
 ];

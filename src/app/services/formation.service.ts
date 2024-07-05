e39aa7bf5340df5d62008f2cb10744a20c8ec6a7
Hugo-walando <hugowalando@gmail.com>
2024-07-05 12:01:51 +0200

    feat: change stocked formation to match new model & add persons

--- src/app/services/formation.service.ts
+++ src/app/services/formation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormationModel } from '../models/formation-model';
+import { FormationModel, PersonModel } from '../models/formation-model';
 
 @Injectable({
   providedIn: 'root',
@@ -7,23 +7,52 @@ import { FormationModel } from '../models/formation-model';
 export class FormationService {
   private formations: FormationModel[] = [
     {
-      id: 1,
-      name: 'Formation 1',
-      start_date: '2023-01-01',
-      end_date: '2023-01-10',
-      place: 'Paris',
-      caps: 'John Doe',
+      id: 0,
+      name: 'Dev Inté P6',
       status: 'en cours',
-    },
-    {
-      id: 2,
-      name: 'Formation 2',
+      type: 'dev inté',
+      place: 'Lens',
+      former_1: {
+        id: 1,
+        name: 'Alice',
+        profilePictureUrl: 'https://placehold.co/90',
+      },
+      former_2: {
+        id: 2,
+        name: 'Bob',
+        profilePictureUrl: 'https://placehold.co/90',
+      },
+      caps: {
+        id: 3,
+        name: 'Charlie',
+        profilePictureUrl: 'https://placehold.co/90',
+      },
       start_date: '2023-02-01',
       end_date: '2023-02-10',
-      place: 'Lyon',
-      caps: 'Jane Smith',
-      status: 'terminé',
+      charge_admin: {
+        id: 4,
+        name: 'Claire',
+        profilePictureUrl: 'https://placehold.co/90',
+      },
+      responsable_campus: {
+        id: 5,
+        name: 'Jean',
+        profilePictureUrl: 'https://placehold.co/90',
+      },
+      responsable_pedagogique: {
+        id: 6,
+        name: 'Rémi',
+        profilePictureUrl: 'https://placehold.co/90',
+      },
     },
+  ];
+  private people: PersonModel[] = [
+    { id: 1, name: 'Alice', profilePictureUrl: 'https://placehold.co/90' },
+    { id: 2, name: 'Bob', profilePictureUrl: 'https://placehold.co/90' },
+    { id: 3, name: 'Charlie', profilePictureUrl: 'https://placehold.co/90' },
+    { id: 4, name: 'Claire', profilePictureUrl: 'https://placehold.co/90' },
+    { id: 5, name: 'Jean', profilePictureUrl: 'https://placehold.co/90' },
+    { id: 6, name: 'Rémi', profilePictureUrl: 'https://placehold.co/90' },
   ];
 
   getFormations(): FormationModel[] {

24c8d4ef7b8f53cb518473eaac51c545c6f3a348
Hugo-walando <hugowalando@gmail.com>
2024-07-05 14:28:08 +0200

    feat: add constructor to get pople from service

--- src/app/components/add-formation-popup/add-formation-popup.component.ts
+++ src/app/components/add-formation-popup/add-formation-popup.component.ts
@@ -1,14 +1,16 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { FormationModel } from '../../models/formation-model';
+import { FormationModel, PersonModel } from '../../models/formation-model';
 import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { FormationService } from '../../services/formation.service';
+import { FormationsComponent } from '../formations/formations.component';
 
 @Component({
   selector: 'app-add-formation-popup',
   templateUrl: './add-formation-popup.component.html',
   styleUrls: ['./add-formation-popup.component.scss'],
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, NgFor, FormationsComponent],
 })
 export class AddFormationPopupComponent {
   @Output() closePopup = new EventEmitter<void>();
@@ -18,13 +20,23 @@ export class AddFormationPopupComponent {
   formation: FormationModel = {
     id: 0,
     name: '',
+    status: 'en cours',
+    type: 'dev inté',
+    place: '',
+    former_1: {} as PersonModel,
+    former_2: {} as PersonModel,
+    caps: {} as PersonModel,
     start_date: '',
     end_date: '',
-    place: '',
-    caps: '',
-    status: 'en cours',
+    charge_admin: {} as PersonModel,
+    responsable_campus: {} as PersonModel,
+    responsable_pedagogique: {} as PersonModel,
   };
+  people: PersonModel[] = [];
 
+  constructor(private formationService: FormationService) {
+    this.people = this.formationService.getPeople();
+  }
   nextStep() {
     this.step++;
   }
@@ -44,11 +56,21 @@ export class AddFormationPopupComponent {
     this.formation = {
       id: 0,
       name: '',
+      status: 'en cours',
+      type: 'dev inté',
+      place: '',
+      former_1: {} as PersonModel,
+      former_2: {} as PersonModel,
+      caps: {} as PersonModel,
       start_date: '',
       end_date: '',
-      place: '',
-      caps: '',
-      status: 'en cours',
+      charge_admin: {} as PersonModel,
+      responsable_campus: {} as PersonModel,
+      responsable_pedagogique: {} as PersonModel,
     };
+  }
+
+  log() {
+    console.log(this.people);
   }
 }

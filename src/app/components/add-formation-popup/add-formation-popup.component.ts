23fc67a8ab49f65f500b9cc7e1bdc54ac146f6d3
Hugo-walando <hugowalando@gmail.com>
2024-07-08 15:25:26 +0200

    feat: change the way the errors are displayed

--- src/app/components/add-formation-popup/add-formation-popup.component.ts
+++ src/app/components/add-formation-popup/add-formation-popup.component.ts
@@ -1,6 +1,13 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormationModel, PersonModel } from '../../models/formation-model';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NgFor, NgIf } from '@angular/common';
 import { FormationService } from '../../services/formation.service';
 import { FormationsComponent } from '../formations/formations.component';
@@ -11,7 +18,15 @@ import { StepperComponent } from '../stepper/stepper.component';
   templateUrl: './add-formation-popup.component.html',
   styleUrls: ['./add-formation-popup.component.scss'],
   standalone: true,
-  imports: [FormsModule, NgIf, NgFor, FormationsComponent, StepperComponent],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    NgFor,
+    FormationsComponent,
+    StepperComponent,
+    CommonModule,
+  ],
 })
 export class AddFormationPopupComponent {
   @Output() closePopup = new EventEmitter<void>();
@@ -19,11 +34,12 @@ export class AddFormationPopupComponent {
 
   step = 1;
   maxSteps = 2;
+  formationForm: FormGroup;
   formation: FormationModel = {
     id: 0,
     name: '',
     status: 'en cours',
-    type: 'dev inté',
+    type: '',
     place: '',
     former_1: {} as PersonModel,
     former_2: {} as PersonModel,
@@ -36,25 +52,68 @@ export class AddFormationPopupComponent {
   };
   people: PersonModel[] = [];
 
-  constructor(private formationService: FormationService) {
+  constructor(
+    private formationService: FormationService,
+    private fb: FormBuilder
+  ) {
     this.people = this.formationService.getPeople();
+    this.formationForm = this.fb.group({
+      name: ['', Validators.required],
+      start_date: ['', Validators.required],
+      end_date: ['', Validators.required],
+      type: ['', Validators.required],
+      place: ['', Validators.required],
+      status: ['en cours', Validators.required],
+      caps: [null, Validators.required],
+      former_1: [null, Validators.required],
+      former_2: [null, Validators.required],
+      charge_admin: [null, Validators.required],
+      responsable_campus: [null, Validators.required],
+      responsable_pedagogique: [null, Validators.required],
+    });
   }
   nextStep() {
-    if (this.step < this.maxSteps) {
+    console.log(this.isStep1Valid());
+    if (this.isStep1Valid()) {
       this.step++;
+    } else {
+      this.markAllAsTouched();
     }
+
+    console.log(this.step);
+  }
+  private isStep1Valid() {
+    return (
+      this.formationForm.get('name')?.valid &&
+      this.formationForm.get('start_date')?.valid &&
+      this.formationForm.get('end_date')?.valid &&
+      this.formationForm.get('type')?.valid &&
+      this.formationForm.get('place')?.valid
+    );
   }
 
   prevStep() {
     if (this.step > 1) {
       this.step--;
     }
+    console.log(this.step);
   }
 
   submitFormation() {
-    this.addFormation.emit(this.formation);
-    this.closePopup.emit();
-    this.resetFormation();
+    if (this.formationForm.valid) {
+      this.addFormation.emit(this.formationForm.value);
+      this.closePopup.emit();
+      this.resetForm();
+    } else {
+      this.markAllAsTouched();
+    }
+  }
+
+  resetForm() {
+    this.step = 1;
+    this.formationForm.reset({
+      status: 'en cours',
+    });
   }
 
   resetFormation() {
@@ -63,7 +122,7 @@ export class AddFormationPopupComponent {
       id: 0,
       name: '',
       status: 'en cours',
-      type: 'dev inté',
+      type: '',
       place: '',
       former_1: {} as PersonModel,
       former_2: {} as PersonModel,
@@ -76,7 +135,7 @@ export class AddFormationPopupComponent {
     };
   }
 
-  log() {
-    console.log(this.people);
+  private markAllAsTouched() {
+    this.formationForm.markAllAsTouched();
   }
 }

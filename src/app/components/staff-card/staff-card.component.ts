dab0a6860f4f6b712428b82446c848e7edd7f9f2
Hugo-walando <hugowalando@gmail.com>
2024-07-09 15:00:33 +0200

    style: change staff card style

--- src/app/components/staff-card/staff-card.component.ts
+++ src/app/components/staff-card/staff-card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PersonModel } from '../../models/formation-model';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-staff-card',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './staff-card.component.html',
-  styleUrl: './staff-card.component.scss'
+  styleUrl: './staff-card.component.scss',
 })
 export class StaffCardComponent {
-
+  @Input() person: PersonModel | null = null;
+  @Input() role = '';
 }

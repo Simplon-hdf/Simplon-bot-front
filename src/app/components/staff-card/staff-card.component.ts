ba6350abdbc7c7273ae30b60603bec76bf248db5
Hugo-walando <hugowalando@gmail.com>
2024-07-09 14:41:26 +0200

    feat: generate staff-card component

---
+++ src/app/components/staff-card/staff-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './staff-card.component.html',
+  styleUrl: './staff-card.component.scss'
+})
+export class StaffCardComponent {
+
+}

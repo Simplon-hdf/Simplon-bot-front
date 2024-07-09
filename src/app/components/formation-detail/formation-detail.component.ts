f03970819048eea79586574f0a9fee138f50710d
Hugo-walando <hugowalando@gmail.com>
2024-07-09 12:22:12 +0200

    feat: generate formation-detail component

---
+++ src/app/components/formation-detail/formation-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formation-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './formation-detail.component.html',
+  styleUrl: './formation-detail.component.scss'
+})
+export class FormationDetailComponent {
+
+}

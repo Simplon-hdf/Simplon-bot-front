0df228df800cddfce1f2e40ad9cceaa69301a4e2
Hugo-walando <hugowalando@gmail.com>
2024-07-10 15:15:22 +0200

    feat: add filter method for searchbar

--- src/app/components/formations/formations.component.ts
+++ src/app/components/formations/formations.component.ts
@@ -4,16 +4,19 @@ import { FormationModel } from '../../models/formation-model';
 import { Router } from '@angular/router';
 import { AddFormationPopupComponent } from '../add-formation-popup/add-formation-popup.component';
 import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formations',
   standalone: true,
-  imports: [NgFor, AddFormationPopupComponent, NgIf],
+  imports: [NgFor, AddFormationPopupComponent, NgIf, FormsModule],
   templateUrl: './formations.component.html',
   styleUrl: './formations.component.scss',
 })
 export class FormationsComponent {
   formations: FormationModel[] = [];
+  filteredFormations: FormationModel[] = [];
+  searchTerm: string = '';
   isPopupVisible = false;
 
   constructor(
@@ -21,6 +24,18 @@ export class FormationsComponent {
     private router: Router
   ) {
     this.formations = this.formationService.getFormations();
+    this.filteredFormations = this.formations;
+  }
+
+  filterFormations(): void {
+    console.log(this.searchTerm);
+    if (!this.searchTerm) {
+      this.filteredFormations = this.formations;
+    } else {
+      this.filteredFormations = this.formations.filter((formation) =>
+        formation.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 
   openPopup() {

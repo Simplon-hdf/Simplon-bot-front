b72e9d3f1b71d2129e81259b71434fc7051b7346
Hugo-walando <hugowalando@gmail.com>
2024-07-16 16:55:21 +0200

    fix: remove comments

--- src/app/models/formation-model.ts
+++ src/app/models/formation-model.ts
@@ -1,20 +1,3 @@
-// export interface FormationModel {
-//   id: number;
-//   name: string;
-//   place: string;
-//   status: string;
-//   type: string;
-//   former_1: PersonModel;
-//   former_2: PersonModel;
-//   caps: PersonModel;
-//   start_date: string;
-//   end_date: string;
-//   charge_admin: PersonModel;
-//   responsable_campus: PersonModel;
-//   responsable_pedagogique: PersonModel;
-//   learners: LearnerModel[];
-// }
-
 export interface BasicInfo {
   name: string;
   start_date: string;

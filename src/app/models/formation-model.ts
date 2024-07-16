3d28259c7fe4b2ad43d0bdc6169ed46b35d9ac66
Hugo-walando <hugowalando@gmail.com>
2024-07-16 16:40:28 +0200

    feat: change models to match new separate component step system

--- src/app/models/formation-model.ts
+++ src/app/models/formation-model.ts
@@ -1,18 +1,43 @@
-export interface FormationModel {
-  id: number;
+// export interface FormationModel {
+//   id: number;
+//   name: string;
+//   place: string;
+//   status: string;
+//   type: string;
+//   former_1: PersonModel;
+//   former_2: PersonModel;
+//   caps: PersonModel;
+//   start_date: string;
+//   end_date: string;
+//   charge_admin: PersonModel;
+//   responsable_campus: PersonModel;
+//   responsable_pedagogique: PersonModel;
+//   learners: LearnerModel[];
+// }
+
+export interface BasicInfo {
   name: string;
-  place: string;
-  status: string;
-  type: string;
-  former_1: PersonModel;
-  former_2: PersonModel;
-  caps: PersonModel;
   start_date: string;
   end_date: string;
+  type: string;
+  place: string;
+  status: string;
+  learners: LearnerModel[];
+}
+
+export interface Staffs {
+  caps: PersonModel;
+  former_1: PersonModel;
+  former_2: PersonModel;
   charge_admin: PersonModel;
   responsable_campus: PersonModel;
   responsable_pedagogique: PersonModel;
-  learners: LearnerModel[];
+}
+
+export interface FormationModel {
+  id: number;
+  basicInfo: BasicInfo;
+  staffs: Staffs;
 }
 
 export interface LearnerModel {

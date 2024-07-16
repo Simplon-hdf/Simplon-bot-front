bf813eaead6e3f4be465af6c65fd9b00ba95635b
Remi Debruyne <remidebruyne@hotmail.com>
2024-07-16 15:53:08 +0200

    feat: make delete button functionnal
    Hide the edit button, I don't know how to implement the logic yet

--- src/app/components/learners-table/learners-table.component.ts
+++ src/app/components/learners-table/learners-table.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LearnerModel } from '../../models/learner-model';
+import { AddLearnersPopupFormComponent } from '../add-learners-popup/add-learners-popup-form/add-learners-popup-form/add-learners-popup-form.component';
+import { AddLearnersPopupComponent } from '../add-learners-popup/add-learners-popup.component';
 
 @Component({
   selector: 'app-learners-table',
   standalone: true,
-  imports: [],
+  imports: [AddLearnersPopupFormComponent, AddLearnersPopupComponent],
   templateUrl: './learners-table.component.html',
   styleUrl: './learners-table.component.scss',
 })
 export class LearnersTableComponent {
   private _learners: LearnerModel[] = [];
+  @Output() editButtonClick = new EventEmitter<boolean>();
 
   @Input()
   public get learners(): LearnerModel[] {
@@ -18,5 +21,14 @@ export class LearnersTableComponent {
 
   public set learners(value: LearnerModel[]) {
     this._learners = value;
+  }
+
+  public deleteLearner(learner: LearnerModel) {
+    const learnerIndex = this.learners.indexOf(learner);
+    this.learners.splice(learnerIndex, 1);
+  }
+
+  public editLearner() {
+    this.editButtonClick.emit(true);
   }
 }

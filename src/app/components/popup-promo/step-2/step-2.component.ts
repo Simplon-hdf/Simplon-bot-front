28905766587c5c3604996dae55aff7f15949542d
Hugo-walando <hugowalando@gmail.com>
2024-07-16 11:40:14 +0200

    feat: add step2 method & import form

--- src/app/components/popup-promo/step-2/step-2.component.ts
+++ src/app/components/popup-promo/step-2/step-2.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormGroupDirective,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
+import { PersonModel } from '../../../models/formation-model';
+import { CommonModule, NgFor } from '@angular/common';
+import { FormationService } from '../../../services/formation.service';
 
 @Component({
   selector: 'app-step-2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor, FormsModule],
   templateUrl: './step-2.component.html',
-  styleUrl: './step-2.component.scss'
+  styleUrl: './step-2.component.scss',
 })
-export class Step2Component {
+export class Step2Component implements OnInit {
+  formationForm!: FormGroup;
+  people: PersonModel[] = [];
 
+  constructor(
+    private rootFormGroup: FormGroupDirective,
+    private formationService: FormationService
+  ) {
+    this.people = this.formationService.getPeople();
+  }
+  ngOnInit(): void {
+    this.formationForm = this.rootFormGroup.control.get('staffs') as FormGroup;
+  }
 }

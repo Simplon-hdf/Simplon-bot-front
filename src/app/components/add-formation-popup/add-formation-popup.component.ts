9b3dff4b462241f1277bb2471802a5610ca95e79
Hugo-walando <hugowalando@gmail.com>
2024-07-16 16:39:59 +0200

    feat: change steps system, change methods to be more scalable & cleaner

--- src/app/components/add-formation-popup/add-formation-popup.component.ts
+++ src/app/components/add-formation-popup/add-formation-popup.component.ts
@@ -12,8 +12,9 @@ import { NgFor, NgIf } from '@angular/common';
 import { FormationService } from '../../services/formation.service';
 import { FormationsComponent } from '../formations/formations.component';
 import { StepperComponent } from '../stepper/stepper.component';
-import { dateRangeValidator } from '../../date-range.validator';
 import { Step1Component } from '../popup-promo/step-1/step-1.component';
+import { Step2Component } from '../popup-promo/step-2/step-2.component';
+import { dateRangeValidator } from '../../validators/date-range.validator';
 
 @Component({
   selector: 'app-add-formation-popup',
@@ -29,6 +30,7 @@ import { Step1Component } from '../popup-promo/step-1/step-1.component';
     StepperComponent,
     CommonModule,
     Step1Component,
+    Step2Component,
   ],
 })
 export class AddFormationPopupComponent {
@@ -38,22 +40,6 @@ export class AddFormationPopupComponent {
   step = 1;
   maxSteps = 2;
   formationForm: FormGroup;
-  formation: FormationModel = {
-    id: 0,
-    name: '',
-    status: '',
-    type: '',
-    place: '',
-    former_1: {} as PersonModel,
-    former_2: {} as PersonModel,
-    caps: {} as PersonModel,
-    start_date: '',
-    end_date: '',
-    charge_admin: {} as PersonModel,
-    responsable_campus: {} as PersonModel,
-    responsable_pedagogique: {} as PersonModel,
-    learners: [],
-  };
   people: PersonModel[] = [];
 
   constructor(
@@ -63,32 +49,63 @@ export class AddFormationPopupComponent {
     this.people = this.formationService.getPeople();
     this.formationForm = this.fb.group(
       {
-        name: [
-          '',
-          [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ]+$/)],
-        ],
-        start_date: ['', Validators.required],
-        end_date: ['', Validators.required],
-        type: ['', Validators.required],
-        place: [
-          '',
-          [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ]+$/)],
-        ],
-        status: [''],
-        caps: [null, Validators.required],
-        former_1: [null, Validators.required],
-        former_2: [null, Validators.required],
-        charge_admin: [null, Validators.required],
-        responsable_campus: [null, Validators.required],
-        responsable_pedagogique: [null, Validators.required],
-        learners: [[]],
+        basicInfo: this.fb.group({
+          name: [
+            '',
+            [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ]+$/)],
+          ],
+          start_date: ['', Validators.required],
+          end_date: ['', Validators.required],
+          type: ['', Validators.required],
+          place: [
+            '',
+            [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ]+$/)],
+          ],
+          status: [''],
+          learners: [[]],
+        }),
+        staffs: this.fb.group({
+          caps: [null, Validators.required],
+          former_1: [null, Validators.required],
+          former_2: [null, Validators.required],
+          charge_admin: [null, Validators.required],
+          responsable_campus: [null, Validators.required],
+          responsable_pedagogique: [null, Validators.required],
+        }),
       },
       { validators: dateRangeValidator() }
     );
     this.updateStatus();
   }
+
   goToNextStep() {
-    this.step++;
+    if (this.isCurrentStepValid()) {
+      console.log('Valid');
+      this.step++;
+    } else {
+      this.markCurrentStepAsTouched();
+      console.log('Not valid');
+    }
+  }
+
+  private isCurrentStepValid(): boolean {
+    const steps = ['basicInfo', 'staffs'];
+    const currentStep = steps[this.step - 1];
+    console.log(this.formationForm.get(currentStep)!.valid);
+    if (currentStep === 'basicInfo') {
+      return (
+        this.formationForm.get(currentStep)!.valid &&
+        !this.formationForm.hasError('dateRange')
+      );
+    }
+    return this.formationForm.get(currentStep)!.valid;
+  }
+
+  private markCurrentStepAsTouched() {
+    const steps = ['basicInfo', 'staffs'];
+    const currentStep = steps[this.step - 1];
+    (this.formationForm.get(currentStep) as FormGroup).markAllAsTouched();
+    console.log('marked');
   }
 
   goToPreviousStep() {
@@ -112,37 +129,17 @@ export class AddFormationPopupComponent {
     const now = new Date();
 
     if (endDate < now) {
-      this.formationForm.get('status')?.setValue('terminé');
+      this.formationForm.get('basicInfo.status')?.setValue('terminé');
     } else if (startDate > now) {
-      this.formationForm.get('status')?.setValue('à venir');
+      this.formationForm.get('basicInfo.status')?.setValue('à venir');
     } else {
-      this.formationForm.get('status')?.setValue('en cours');
+      this.formationForm.get('basicInfo.status')?.setValue('en cours');
     }
   }
 
   resetForm() {
     this.step = 1;
     this.formationForm.reset();
-  }
-
-  resetFormation() {
-    this.step = 1;
-    this.formation = {
-      id: 0,
-      name: '',
-      status: '',
-      type: '',
-      place: '',
-      former_1: {} as PersonModel,
-      former_2: {} as PersonModel,
-      caps: {} as PersonModel,
-      start_date: '',
-      end_date: '',
-      charge_admin: {} as PersonModel,
-      responsable_campus: {} as PersonModel,
-      responsable_pedagogique: {} as PersonModel,
-      learners: [],
-    };
   }
 
   private markAllAsTouched() {

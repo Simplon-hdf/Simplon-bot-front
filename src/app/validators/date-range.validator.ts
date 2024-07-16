989736cbb1bc9978011b6f24cecb0a4723f263bb
Hugo-walando <hugowalando@gmail.com>
2024-07-16 16:29:35 +0200

    feat: add validator in seperate folder

---
+++ src/app/validators/date-range.validator.ts
@@ -0,0 +1,18 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function dateRangeValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const startDate = control.get('basicInfo.start_date')?.value;
+    const endDate = control.get('basicInfo.end_date')?.value;
+
+    if (!startDate || !endDate) {
+      return null;
+    }
+
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    console.log(start, end);
+    console.log(start <= end ? null : { dateRange: true });
+    return start <= end ? null : { dateRange: true };
+  };
+}
